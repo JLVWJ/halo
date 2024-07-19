df78520b7764588db686ff9b527759d74e030832
lvweijie <lvweijie@qudian.com>
2024-07-19 21:10:07 +0800

    Entity补充字段

--- framework/halo-core/src/main/java/com/lvwj/halo/core/domain/entity/Entity.java
+++ framework/halo-core/src/main/java/com/lvwj/halo/core/domain/entity/Entity.java
@@ -1,16 +1,14 @@
 package com.lvwj.halo.core.domain.entity;
 
 import com.lvwj.halo.common.models.entity.IEntity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Setter
 @Getter
 @ToString(callSuper = true)
 @SuperBuilder(toBuilder = true)
@@ -19,20 +17,12 @@ public abstract class Entity<ID extends Serializable> implements IEntity<ID> {
 
   private ID id;
 
-  //暂存PO的字段数据，无业务用途
+  private String createBy;
+  private String updateBy;
+  private String deleteBy;
   private LocalDateTime createTime;
   private LocalDateTime updateTime;
-  /**
-   * 删除人
-   */
-  private String deleteBy;
-  /**
-   * 删除时间
-   */
   private LocalDateTime deleteTime;
-  /**
-   * 是否删除
-   */
   private Integer isDelete;
 
 

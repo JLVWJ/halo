a542cfa8320d8b324cc533240ea3cde2751678c5
lvweijie <lvweijie@qudian.com>
2024-08-29 13:57:28 +0800

    完善工具类

--- framework/halo-common/src/main/java/com/lvwj/halo/common/constants/SystemConstant.java
+++ framework/halo-common/src/main/java/com/lvwj/halo/common/constants/SystemConstant.java
@@ -18,17 +18,17 @@ public class SystemConstant {
     /**
      * 编码
      */
-    String UTF_8 = "UTF-8";
+    public static String UTF_8 = "UTF-8";
 
     /**
      * contentType
      */
-    String CONTENT_TYPE_NAME = "Content-type";
+    public static String CONTENT_TYPE_NAME = "Content-type";
 
     /**
      * JSON 资源
      */
-    String CONTENT_TYPE = "application/json;charset=utf-8";
+    public static String CONTENT_TYPE = "application/json;charset=utf-8";
 
 
     public static final String TRACE_ID = "traceId";

8b5de26adfdb3beb6f1717ad27570cb713818c91
lvweijie <lvweijie@qudian.com>
2024-09-02 16:33:07 +0800

    swagger3 submit

---
+++ framework/halo-common/src/main/java/com/lvwj/halo/common/constants/PropertyKeyConstant.java
@@ -0,0 +1,19 @@
+package com.lvwj.halo.common.constants;
+
+/**
+ * 属性常量
+ *
+ * @author lvweijie
+ * @date 2024年09月02日 15:37
+ */
+public class PropertyKeyConstant {
+
+    private PropertyKeyConstant() {
+    }
+
+    public static final String APP_ID = "app.id";
+    public static final String APP_NAME = "spring.application.name";
+
+    public static final String SERVER_CONTEXT_PATH = "server.servlet.context-path";
+    public static final String SERVER_PORT = "server.port";
+}
--- framework/halo-starters/halo-starter-swagger3/src/main/java/com/lvwj/halo/swagger3/config/Swagger3Configration.java
+++ framework/halo-starters/halo-starter-swagger3/src/main/java/com/lvwj/halo/swagger3/config/Swagger3Configration.java
@@ -1,22 +1,28 @@
 package com.lvwj.halo.swagger3.config;
 
+import com.lvwj.halo.swagger3.config.properties.Swagger3Properties;
 import io.swagger.v3.oas.models.OpenAPI;
+import jakarta.annotation.Resource;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author lvweijie
  * @date 2024年09月02日 14:26
  */
 @AutoConfiguration
+@EnableConfigurationProperties(value = {Swagger3Properties.class})
 public class Swagger3Configration {
 
+    @Resource
+    private Swagger3Properties swagger3Properties;
 
 
-    public OpenAPI openAPI(){
-
-
+    @Bean
+    public OpenAPI openAPI() {
         OpenAPI openAPI = new OpenAPI();
-
-        return null;
+        openAPI.setInfo(swagger3Properties.toInfo());
+        return openAPI;
     }
 }

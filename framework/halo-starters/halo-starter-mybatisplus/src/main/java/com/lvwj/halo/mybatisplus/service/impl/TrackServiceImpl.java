423a5f733d22760a03338c47b875eb18f9ab4e2e
lvweijie <lvweijie@qudian.com>
2024-09-13 18:05:45 +0800

    变更追踪：支持PO字段是复杂Json格式的

--- framework/halo-starters/halo-starter-mybatisplus/src/main/java/com/lvwj/halo/mybatisplus/service/impl/TrackServiceImpl.java
+++ framework/halo-starters/halo-starter-mybatisplus/src/main/java/com/lvwj/halo/mybatisplus/service/impl/TrackServiceImpl.java
@@ -9,7 +9,6 @@ import com.lvwj.halo.core.track.impl.ThreadLocalTrackManager;
 import com.lvwj.halo.mybatisplus.entity.EntityHolder;
 import com.lvwj.halo.mybatisplus.mapper.CustomMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.javers.core.Changes;
 import org.javers.core.ChangesByObject;
 import org.javers.core.diff.Change;
 import org.javers.core.diff.changetype.*;
@@ -20,9 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toSet;
 
 /**
  * 追踪服务实现
@@ -53,7 +52,7 @@ public abstract class TrackServiceImpl<M extends CustomMapper<T>, T extends IEnt
             Long version = verObj != null ? Long.parseLong(verObj.toString()) + 1 : 0;
             EntityHolder.setFieldValue(t, EntityHolder.VERSION, version);
         }
-        Changes changes = this.trackManager.changeDiff(t);
+        List<ChangesByObject> changes = this.trackManager.changeDiffByObject(t);
         doSave(changes);
         //this.trackManager.merge(t); 更新快照数据，为了可以多次saveByTrack
         //事务完成后，解除变更追踪
@@ -74,7 +73,7 @@ public abstract class TrackServiceImpl<M extends CustomMapper<T>, T extends IEnt
                 EntityHolder.setFieldValue(t, EntityHolder.VERSION, version);
             }
         }
-        Changes changes = this.trackManager.changeDiff(list, getEntityClass());
+        List<ChangesByObject> changes = this.trackManager.changeDiffByObject(list, getEntityClass());
         doSave(changes);
         //this.trackManager.merge(list); 更新快照数据，为了可以多次saveByTrack
         //事务完成后，解除变更追踪
@@ -88,8 +87,8 @@ public abstract class TrackServiceImpl<M extends CustomMapper<T>, T extends IEnt
             return;
         }
         if (entityClassIsJoin()) {
-            Changes changes = this.trackManager.deleteDiff(t);
-            doDelete(changes);
+            List<ChangesByObject> changes = this.trackManager.deleteDiffByObject(t);
+            doDelete(getDeleteMap(changes));
         } else {
             super.removeById(t);
         }
@@ -103,8 +102,8 @@ public abstract class TrackServiceImpl<M extends CustomMapper<T>, T extends IEnt
             return;
         }
         if (entityClassIsJoin()) {
-            Changes changes = this.trackManager.deleteDiff(list, getEntityClass());
-            doDelete(changes);
+            List<ChangesByObject> changes = this.trackManager.deleteDiffByObject(list, getEntityClass());
+            doDelete(getDeleteMap(changes));
         } else {
             super.removeBatchByIds(list);
         }
@@ -121,39 +120,46 @@ public abstract class TrackServiceImpl<M extends CustomMapper<T>, T extends IEnt
         if (Func.isEmpty(changes)) {
             return;
         }
-
-
-
-    }
-
-    private void doSave(Changes changes) {
-        if (null == changes || changes.isEmpty()) {
-            return;
-        }
         Map<String, List<Change>> createMap = new HashMap<>();
-        Map<String, List<Change>> deleteMap = new HashMap<>();
+        Map<String, List<Object>> deleteMap = new HashMap<>();
         Map<String, Map<Object, List<Change>>> updateMap = new HashMap<>();
-        for (Change change : changes) {
-            String typeName = change.getAffectedGlobalId().getTypeName();
-            if (isCreate(change)) {
+
+        for (ChangesByObject change : changes) {
+            if (!(change.getGlobalId() instanceof InstanceId instanceId))
+                continue;
+            String typeName = instanceId.getTypeName();
+            Object id = instanceId.getCdoId();
+            if (Func.isNotEmpty(change.getNewObjects())) {
                 List<Change> list = createMap.computeIfAbsent(typeName, k -> new ArrayList<>());
-                list.add(change);
-            } else if (isDelete(change)) {
-                List<Change> list = deleteMap.computeIfAbsent(typeName, k -> new ArrayList<>());
-                list.add(change);
-            }else if (isUpdate(change)) {
-                Map<Object, List<Change>> map = updateMap.computeIfAbsent(typeName, k -> new HashMap<>());
-                List<Change> list = map.computeIfAbsent(getEntityId(change), k -> new ArrayList<>());
-                list.add(change);
+                list.add(change.getNewObjects().get(0));
+            } else if (Func.isNotEmpty(change.getObjectsRemoved())) {
+                List<Object> ids = deleteMap.computeIfAbsent(typeName, k -> new ArrayList<>());
+                if (!ids.contains(id)) {
+                    ids.add(id);
+                }
+            } else if (Func.isNotEmpty(change.getPropertyChanges())) {
+                for (PropertyChange propertyChange : change.getPropertyChanges()) {
+                    if (!(propertyChange.getAffectedGlobalId() instanceof InstanceId parentId)
+                            || !parentId.getTypeName().equals(typeName)
+                            || !ObjectUtil.nullSafeEquals(parentId.getCdoId(), id)
+                            || checkIsInstanceId(propertyChange.getLeft())
+                            || checkIsInstanceId(propertyChange.getRight()))
+                        continue;
+                    Map<Object, List<Change>> map = updateMap.computeIfAbsent(typeName, k -> new HashMap<>());
+                    List<Change> list = map.computeIfAbsent(id, k -> new ArrayList<>());
+                    list.add(propertyChange);
+                }
             }
         }
+
         doCreate(createMap);
         doUpdate(updateMap);
         doDelete(deleteMap);
     }
 
+
     private void doCreate(Map<String, List<Change>> map) {
-        if (map.isEmpty()) {
+        if (Func.isEmpty(map)) {
             return;
         }
         List<Object> entities = new ArrayList<>();
@@ -172,18 +178,15 @@ public abstract class TrackServiceImpl<M extends CustomMapper<T>, T extends IEnt
         }
     }
 
-    private void doDelete(Map<String, List<Change>> map) {
-        for (Map.Entry<String, List<Change>> entry : map.entrySet()) {
-            Set<Object> entityIds = entry.getValue().stream().map(this::getEntityId).collect(toSet());
-            if (!CollectionUtils.isEmpty(entityIds)) {
-                CustomMapper mapper = EntityHolder.getMapper(entry.getKey());
-                mapper.deleteByIds(entityIds);
-            }
+    private void doDelete(Map<String, List<Object>> map) {
+        for (Map.Entry<String, List<Object>> entry : map.entrySet()) {
+            CustomMapper mapper = EntityHolder.getMapper(entry.getKey());
+            mapper.deleteByIds(entry.getValue());
         }
     }
 
     private void doUpdate(Map<String, Map<Object, List<Change>>> map) {
-        if (map.isEmpty()) {
+        if (Func.isEmpty(map)) {
             return;
         }
         List<Object> entities = new ArrayList<>();
@@ -195,14 +198,14 @@ public abstract class TrackServiceImpl<M extends CustomMapper<T>, T extends IEnt
                 }
                 Object entity = null;
                 for (Change change : itemEntry.getValue()) {
-                    PropertyChange<Object> valueChange = (PropertyChange<Object>) change;
-                    EntityHolder.EntityField entityField = EntityHolder.getEntityField(entityClass, valueChange.getPropertyName());
-                    if (entityField.allowUpdate(valueChange.getRight())) {
+                    PropertyChange<Object> propertyChange = (PropertyChange<Object>) change;
+                    EntityHolder.EntityField entityField = EntityHolder.getEntityField(entityClass, propertyChange.getPropertyName());
+                    if (entityField.allowUpdate(propertyChange.getRight())) {
                         if (entity == null) {
                             entity = BeanUtils.instantiateClass(entityClass);
                             EntityHolder.setFieldValue(entity, "id", itemEntry.getKey());
                         }
-                        EntityHolder.setFieldValue(entity, valueChange.getPropertyName(), valueChange.getRight());
+                        EntityHolder.setFieldValue(entity, propertyChange.getPropertyName(), propertyChange.getRight());
                     }
                 }
                 if (null != entity) {
@@ -216,32 +219,13 @@ public abstract class TrackServiceImpl<M extends CustomMapper<T>, T extends IEnt
         }
     }
 
-    private void doDelete(Changes changes) {
-        if (null == changes || changes.isEmpty()) {
-            return;
-        }
-        Map<String, List<Change>> map = changes.stream().filter(this::isDelete).collect(groupingBy(s -> s.getAffectedGlobalId().getTypeName()));
-        doDelete(map);
-    }
 
     private boolean isCreate(Change change) {
         return change instanceof NewObject;
     }
 
-    private boolean isUpdate(Change change) {
-        return change instanceof ValueChange && !(change instanceof InitialValueChange) && !(change instanceof TerminalValueChange);
-    }
-
     private boolean isDelete(Change change) {
         return change instanceof ObjectRemoved;
-    }
-
-    private boolean existsInMap(Map<String, List<Change>> map, Change change) {
-        if (Func.isEmpty(map) || null == change) return false;
-        String typeName = change.getAffectedGlobalId().getTypeName();
-        Object entityId = getEntityId(change);
-        List<Change> list = map.get(typeName);
-        return Func.isNotEmpty(list) && list.stream().anyMatch(s -> ObjectUtil.nullSafeEquals(getEntityId(s), entityId));
     }
 
     /**
@@ -258,5 +242,17 @@ public abstract class TrackServiceImpl<M extends CustomMapper<T>, T extends IEnt
             //其他全部当做ValueObject对象处理
             return ((InstanceId) ((ValueObjectId) change.getAffectedGlobalId()).getOwnerId()).getCdoId();
         }
+    }
+
+    private Map<String, List<Object>> getDeleteMap(List<ChangesByObject> changes) {
+        if (Func.isEmpty(changes)) return Collections.emptyMap();
+        return changes.stream().filter(s -> Func.isNotEmpty(s.getObjectsRemoved()) && s.getGlobalId() instanceof InstanceId)
+                .collect(groupingBy(s -> s.getGlobalId().getTypeName(), Collectors.mapping(a -> ((InstanceId) a.getGlobalId()).getCdoId(), Collectors.toList())));
+    }
+
+    private boolean checkIsInstanceId(Object value) {
+        return null != value
+                && ((value instanceof Collection<?> collection && collection.iterator().next() instanceof InstanceId)
+                    || value instanceof InstanceId);
     }
 }
